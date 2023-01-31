5a53942dc2f0b862da0fd0c96fcad8ef625bce02
wxxhyeong <wxxhyeong@gmail.com>
2023-01-31 16:46:32 +0900

    sds/boj2003(수들의합2)/1월31일

--- src/boj2003.java
+++ src/boj2003.java
@@ -1,5 +1,12 @@
-public class boj2003 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class boj2003 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(stk.nextToken());
     }
 }
