cc3c3338f2390436305668bc27f195b36e6ea8e0
wxxhyeong <wxxhyeong@gmail.com>
2023-02-01 23:57:22 +0900

    temp

---
+++ src/Day1_Algorithm_Basic/boj9663.java
@@ -0,0 +1,11 @@
+/*
+백준 9663. n-queen
+ */
+
+package Day1_Algorithm_Basic;
+
+public class boj9663 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Day3_Data_Structure/boj1991.java
@@ -0,0 +1,10 @@
+/*
+백준 1991. 트리순회
+ */
+package Day3_Data_Structure;
+
+public class boj1991 {
+    public static void main(String[] args) {
+
+    }
+}
