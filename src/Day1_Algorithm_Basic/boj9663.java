3b122d943f39d650f1d01d2c748603c7c101efc2
wxxhyeong <wxxhyeong@gmail.com>
2023-02-02 20:06:42 +0900

    sds/day1/boj9663(n-queen)/2월2일/설명추가

--- src/Day1_Algorithm_Basic/boj9663.java
+++ src/Day1_Algorithm_Basic/boj9663.java
@@ -10,23 +10,25 @@ import java.io.InputStreamReader;
 public class boj9663 {
 
     static int cnt, n;
-    static int[] visit_y;
-    static int[] visit_l;
-    static int[] visit_r;
+    static int[] visit_y;  // 열의 퀸 유무 체크 배열
+    static int[] visit_l;  // 왼쪽 방향으로 내려가는 대각선 퀸 유무 체크 배열
+    static int[] visit_r;  // 오른쪽 방향으로 내려가는 대각선 퀸 유무 체크 배열
 
     static void back(int x) {
-        //System.out.println(x);
+        // 행 x의 값이 n인 경우, 즉 체스판 가장 아래까지 퀸이 배치된 경우 경우의 수를 증가시키고 종료
         if (x == n) {
             cnt++;
             return;
         }
 
+        // x번째 행의 y번째 열을 탐색
         for (int y = 0; y < n; y++) {
+            // 순서대로 열, 대각선 오, 왼 방향의 퀸 유무 확인
             if (visit_y[y] == 0 && visit_r[x - y + (n + 1)] == 0 && visit_l[x + y] == 0) {
                 visit_y[y] = 1;
                 visit_l[x+y] = 1;
                 visit_r[x - y + (n + 1)] = 1;
-                back(x + 1);
+                back(x + 1); // 퀸 유무 확인 후, 다음 행 탐색
                 visit_y[y] = 0;
                 visit_l[x+y] = 0;
                 visit_r[x - y + (n + 1)] = 0;
