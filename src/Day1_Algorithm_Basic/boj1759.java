17a9f9388cfe2b15c9d7274f88c25e3c6342dc34
wxxhyeong <wxxhyeong@gmail.com>
2023-02-08 23:48:59 +0900

    sds/day1/boj1759(암호 만들기)/2월8일/temp

---
+++ src/Day1_Algorithm_Basic/boj1759.java
@@ -0,0 +1,20 @@
+package Day1_Algorithm_Basic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj1759 {
+    static int l;
+    static int c;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        l = Integer.parseInt(stk.nextToken());
+        c = Integer.parseInt(stk.nextToken());
+
+
+    }
+}
